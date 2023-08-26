b1bce5e0b8bbd776c82f4244714198a588e35243
Antarpreet <pnarain82@gmail.com>
2023-08-26 12:04:25 +0930

    book

--- src/pages/books/Books.js
+++ src/pages/books/Books.js
@@ -1,12 +1,21 @@
 import React from "react";
 import AdminLayouts from "../../components/Layouts/AdminLayouts";
-
+import { Link, useNavigate } from "react-router-dom";
+import { Button } from "react-bootstrap";
+import BookTable from "../../components/book/BookTable";
+import { useDispatch } from "react-redux";
+import { deleteBookAction } from "./bookAction";
 function Books() {
   return (
     <>
       <AdminLayouts>
         <h3>Books</h3>
         <hr />
+        <div>
+          <Link to="/new-book" />
+          <Button>Add Book</Button>
+        </div>
+        <BookTable />
       </AdminLayouts>
     </>
   );
--- src/store.js
+++ src/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import bookReducer from "./pages/books/bookSlice";
+
 import adminReducer from "./user/userSlice";
 export const store = configureStore({
   reducer: {
     adminInfo: adminReducer,
+    book: bookReducer,
   },
 });
